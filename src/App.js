4a7ab27323de024fd3aa29f168349ac2212d12dd
Francisco Chensan <chensan@u.nus.edu>
2021-01-08 23:20:51 +0800

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+
+
+
+import {Typography, Row, Col} from 'antd';
+
+import {WhispersFeed, MessageBox, Sidemenu} from './components';
+import Sider from 'antd/lib/layout/Sider';
+
+
+const { Title } = Typography;
+
+let testData = [
+  {
+    sender: 'd-100',
+    isAnon: false,
+    content: 'acknowleasfasdfdge from d100',
+    receiver: 'd-104'
+  },
+  {
+    sender: 'd-100',
+    isAnon: false,
+    content: 'acknowledasfasge from d100',
+    receiver: 'd-104'
+  },
+  {
+    sender: 'd-100',
+    isAnon: false,
+    content: 'acknowledge from d100',
+    receiver: 'd-104'
+  },
+  {
+    sender: 'd-100',
+    isAnon: false,
+    content: 'acknowledge from d100',
+    receiver: 'd-104'
+  },
+  {
+    sender: 'd-100',
+    isAnon: false,
+    content: 'acknowledge from d100',
+    receiver: 'd-104'
+  },
+  {
+    sender: 'd-100',
+    isAnon: false,
+    content: 'acknowledge from d100',
+    receiver: 'd-104'
+  },
+  {
+    sender: 'd-100',
+    isAnon: false,
+    content: 'acknowledge from d100',
+    receiver: 'd-104'
+  },
+];
+let testCurrentUserId = 'd-104';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row>
+        <Col span={4}> 
+          <div style={{position: 'fixed'}}>
+            <Sidemenu username="Jack"/>
+          </div>
+        </Col>
+        <Col span={10}>
+          <div style={{overflow:'scroll', paddingTop: '20px'}}>
+            <Title level={3}>Whispers</Title>
+            <WhispersFeed style={{overflow:'scroll'}} whispers={testData} currentUserId={testCurrentUserId}/>
+          </div>
+        </Col>
+        <Col span={10}>
+          <div style={{position: 'fixed', paddingTop: '20px', paddingLeft: '20px', marginRight: '20px', width: '100%'}}>
+            <MessageBox />
+          </div>
+        </Col>
+      </Row>
+
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Whisper from './Whisper.js';
+import WhispersFeed from './WhispersFeed';
+import MessageBox from './MessageBox';
+import Sidemenu from './Sidemenu';
+
+export {
+    Whisper,
+    WhispersFeed,
+    MessageBox,
+    Sidemenu
+};
